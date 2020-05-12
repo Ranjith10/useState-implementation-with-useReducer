69b5256776ce10e96733c6e09a15fc5716238db4
Ranjith kumar <ranjit4072@gmail.com>
2020-05-12 18:33:32 +0530

    Implemented useState with useReducer
    Used React.useReducer to mimic React.useState by a user-defined useState function.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./styles.css";
 
+const useState = initialValue => {
+  return useReducer(useStateReducer, initialValue, useStateInitializer);
+};
+
+const useStateReducer = (prevState, dispatch) => {
+  if (dispatch === "increment") {
+    return prevState + 1;
+  } else {
+    return prevState - 1;
+  }
+};
+
+const useStateInitializer = initialValue => initialValue;
+
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  //Directly set state from prevState value
+  const handleCount = action => setCount(action);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      {count}
+      <div
+        onClick={() => handleCount("increment")}
+        style={{
+          border: "1px solid",
+          width: 150,
+          cursor: "pointer",
+          background: "#B6D7A8"
+        }}
+      >
+        Click to Increment
+      </div>
+      <div
+        onClick={() => handleCount("decrement")}
+        style={{
+          border: "1px solid",
+          width: 150,
+          cursor: "pointer",
+          background: "#F4CCCC",
+          marginTop: 10
+        }}
+      >
+        Click to Decrement
+      </div>
+    </>
   );
 }
